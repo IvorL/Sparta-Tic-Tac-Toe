ce99aba5b3887e393c69162ce9d89e205cf593c0
Ivor Lishman <tlishman@spartaglobal.com>
2018-08-12 13:39:16 +0100

    Added a click counter and functionality so that on even clicks a dot will be placed in the box and on odd clicks a square will be placed in the box

--- index.js
+++ index.js
@@ -3,18 +3,32 @@
 // This will 'wrap' the javascript code
 document.addEventListener("DOMContentLoaded", function() {
 
-// Select button elements
-// This will be an array
-var buttons = document.getElementsByTagName('button')
+  // variable to store the number of clicks
+  var clickTotal = 0;
 
-// Create an event listener for each button
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", changeElementStyle);
-}
-// changes the element style in order to 'place' as square or a dot in the box
-function changeElementStyle() {
-  document.getElementById('innerStyle').className = "dot";
-}
+  // Select button elements
+  // This will be an array
+  var buttons = document.getElementsByTagName('button')
+
+  // Create an event listener for each button in the buttons array
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", changeElementStyle);
+  }
+
+  // changes the element style in order to 'place' a square or a dot in the box
+  function changeElementStyle() {
+    clickTotal++;
+    // For even clicks, place a square in the box
+    if (clickTotal % 2 == 0) {
+    // select the 'div' element inside the button and change the class name to alter styling.
+    // 'this' is a reference to the button that has been clicked on
+      this.getElementsByTagName('div')[0].className = "square";
+    } else {
+        this.getElementsByTagName('div')[0].className = "dot"
+      }
+  }
+
+  // Win conditions:
 
 
 }); // end of DOMContentLoaded event listener
